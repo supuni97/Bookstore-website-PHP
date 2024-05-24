b1e91f8da7aef9b5824c7f356e3fd9b5c3795a2b
supuni <sitharabandara1997@gmail.com>
2024-05-24 19:04:44 +0530

    Added shopping cart page

--- shopping/single.php
+++ shopping/single.php
@@ -2,7 +2,7 @@
 <?php require "../config/config.php"; ?>
 
 <?php
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $pro_id = $_POST['pro_id'];
     $pro_name = $_POST['pro_name'];
     $pro_image = $_POST['pro_image'];
@@ -28,8 +28,11 @@ if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
     //checkin for product in cart
-    $select = $conn->query(("SELECT * FROM cart WHERE pro_id = '$id' AND user_id='$_SESSION[user_id]' "));
-    $select->execute();
+    if (isset($_SESSION['user_id'])) {
+        $select = $conn->query(("SELECT * FROM cart WHERE pro_id = '$id' AND user_id='$_SESSION[user_id]' "));
+        $select->execute();
+    }
+
 
     //getting data for every product
     $row = $conn->prepare("SELECT * FROM products WHERE status=1 AND id=:id");
@@ -47,30 +50,30 @@ if (isset($_GET['id'])) {
 ?>
 
 <div class="container mt-5 mb-5">
-    <div class="row d-flex justify-content-center">
+    <div class="row d-flex justify-content-center mt-4">
         <div class="col-md-10">
             <div class="card">
                 <div class="row">
                     <div class="col-md-6">
                         <div class="images p-3">
-                            <div class="text-center p-4"> 
-                                <img id="main-image" src="../images/<?php echo $product->image ?>" width="250" /> 
+                            <div class="text-center p-4">
+                                <img id="main-image" src="../images/<?php echo $product->image ?>" width="250" />
                             </div>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="product p-4">
                             <div class="d-flex justify-content-between align-items-center">
-                                <div class="d-flex align-items-center"> 
+                                <div class="d-flex align-items-center">
                                     <a href="http://localhost:3000/index.php" class="ml-1 btn btn-primary">
                                         <i class="fa fa-long-arrow-left"></i> Back
-                                    </a> 
-                                </div> 
+                                    </a>
+                                </div>
                                 <i class="fa fa-shopping-cart text-muted"></i>
                             </div>
                             <div class="mt-4 mb-3">
                                 <h5 class="text-uppercase"><?php echo $product->name ?></h5>
-                                <div class="price d-flex flex-row align-items-center"> 
+                                <div class="price d-flex flex-row align-items-center">
                                     <span class="act-price">$<?php echo $product->price ?></span>
                                 </div>
                             </div>
@@ -94,17 +97,18 @@ if (isset($_GET['id'])) {
                                 <div class="">
                                     <input value="<?php echo $product->file; ?>" type="hidden" class="form-control" name="pro_file">
                                 </div>
-                                <div class="">
-                                    <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id']; ?>" class="form-control">
-                                </div>
-
+                                <?php if (isset($_SESSION['user_id'])) : ?>
+                                    <div class="">
+                                        <input type="hidden" name="user_id" value="<?php echo $_SESSION['user_id']; ?>" class="form-control">
+                                    </div>
+                                <?php endif; ?>
                                 <div class="cart mt-4 align-items-center">
-                                    <?php if($select-> rowCount() >0 ): ?>
-                                    <button class="btn btn-primary text-uppercase mr-2 px-4" id="submit" name="submit" disabled type="submit"><i class="fas fa-shopping-cart"></i> Added to cart</button>
-                                   
-                                    <?php else: ?>
-                                    <button class="btn btn-primary text-uppercase mr-2 px-4" id="submit" name="submit" type="submit"><i class="fas fa-shopping-cart"></i> Add to cart</button>
-
+                                    <?php if (isset($_SESSION['user_id'])) : ?>
+                                        <?php if ($select->rowCount() > 0) : ?>
+                                            <button class="btn btn-primary text-uppercase mr-2 px-4" id="submit" name="submit" disabled type="submit"><i class="fas fa-shopping-cart"></i> Added to cart</button>
+                                        <?php else : ?>
+                                            <button class="btn btn-primary text-uppercase mr-2 px-4" id="submit" name="submit" type="submit"><i class="fas fa-shopping-cart"></i> Add to cart</button>
+                                        <?php endif; ?>
                                     <?php endif; ?>
                                 </div>
                             </form>
@@ -126,7 +130,7 @@ if (isset($_GET['id'])) {
 
             $.ajax({
                 type: "post",
-                url: "single.php?id=<?php echo $id;?>",
+                url: "single.php?id=<?php echo $id; ?>",
                 data: formdata,
                 success: function() {
                     alert("Added to cart successfully");
