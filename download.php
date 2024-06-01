f0fdb633c13e7effaaae33c00f6c28ed6c82de13
supuni <sitharabandara1997@gmail.com>
2024-06-01 19:21:14 +0530

    Completed cart

--- download.php
+++ download.php
@@ -2,8 +2,10 @@
 require "includes/header.php";
 require "config/config.php";
 
-// Start the session to use session variables
-session_start();
+// Check if session is not already started
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 
 // Check if user is logged in
 if (!isset($_SESSION['user_id'])) {
@@ -12,7 +14,7 @@ if (!isset($_SESSION['user_id'])) {
 
 try {
     // Fetch all products in the user's cart
-    $select = $conn->prepare("SELECT * FROM CART WHERE user_id = :user_id");
+    $select = $conn->prepare("SELECT * FROM cart WHERE user_id = :user_id");
     $select->bindParam(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
     $select->execute();
     $allProducts = $select->fetchAll(PDO::FETCH_OBJ);
@@ -31,7 +33,7 @@ try {
     
     // Add each product file to the zip
     foreach ($allProducts as $product) {
-        $filePath = "books/".$product->pro_file;
+        $filePath = "books/" . $product->pro_file;
         if (file_exists($filePath)) {
             $zip->addFile($filePath, basename($filePath));
         } else {
@@ -42,7 +44,7 @@ try {
     
     // Close the zip archive
     $zip->close();
-    
+
     // Set headers to initiate file download
     header('Content-Type: application/zip');
     header('Content-Disposition: attachment; filename="' . $zipname . '"');
@@ -55,6 +57,12 @@ try {
     
     // Optionally delete the zip file after download
     unlink($zipname);
+
+    // Delete all products in the user's cart
+    $delete = $conn->prepare("DELETE FROM cart WHERE user_id = :user_id");
+    $delete->bindParam(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
+    $delete->execute();
+
 } catch (Exception $e) {
     // Handle any exceptions
     die("Error: " . $e->getMessage());
--- index.php
+++ index.php
@@ -10,25 +10,51 @@ $allRows = $rows->fetchAll(PDO::FETCH_OBJ);
 
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product Listing</title>
+    <script type="text/javascript">
+        function startDownload() {
+            // Start the download in a hidden iframe
+            var iframe = document.createElement("iframe");
+            iframe.style.display = "none";
+            iframe.src = "download.php"; // URL to the PHP script handling the zip download
+            document.body.appendChild(iframe);
 
-<div class="row mt-5">
-    <?php foreach ($allRows as $product) : ?>
-        <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
-            <div class="card">
-                <img height="213px" class="card-img-top" src="images/<?php echo $product->image; ?>">
-                <div class="card-body">
-                    <h5 class="d-inline"><b><?php echo $product->name; ?></b> </h5>
-                    <h5 class="d-inline">
-                        <div class="text-muted d-inline">($<?php echo $product->price; ?>/item)</div>
-                    </h5>
-                    <p><?php echo substr($product->description, 0, 120)  ?> </p>
-                    <a href="http://localhost:3000/shopping/single.php?id=<?php echo $product->id; ?>" class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>
-
+            // Redirect after a short delay
+            setTimeout(function() {
+                window.location.href = "index.php";
+            }, 3000); // 3-second delay to allow download to start
+        }
+    </script>
+</head>
+<body>
+    <div class="row mt-5">
+        <?php foreach ($allRows as $product) : ?>
+            <div class="col-lg-4 col-md-6 col-sm-10 offset-md-0 offset-sm-1">
+                <div class="card">
+                    <img height="213px" class="card-img-top" src="images/<?php echo $product->image; ?>">
+                    <div class="card-body">
+                        <h5 class="d-inline"><b><?php echo $product->name; ?></b></h5>
+                        <h5 class="d-inline">
+                            <div class="text-muted d-inline">($<?php echo $product->price; ?>/item)</div>
+                        </h5>
+                        <p><?php echo substr($product->description, 0, 120) ?> </p>
+                        <a href="http://localhost:3000/shopping/single.php?id=<?php echo $product->id; ?>" class="btn btn-primary w-100 rounded my-2"> More<i class="fas fa-arrow-right"></i> </a>
+                    </div>
                 </div>
             </div>
+            <br>
+        <?php endforeach; ?>
+    </div>
+
+    <div class="row mt-5">
+        <div class="col-12">
+            <button onclick="startDownload()" class="btn btn-success">Download All Products</button>
         </div>
-        <br>
-    <?php endforeach; ?>
-</div>
+    </div>
+</body>
+</html>
 
 <?php require "includes/footer.php"; ?>
