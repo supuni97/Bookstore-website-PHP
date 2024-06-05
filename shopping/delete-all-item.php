0013951638e0b6920f4b6e7e196d2a4c461fca0a
supuni <sitharabandara1997@gmail.com>
2024-06-05 19:06:57 +0530

    Updated some pages by adding prevent direct url access

--- shopping/delete-all-item.php
+++ shopping/delete-all-item.php
@@ -2,9 +2,16 @@
 <?php require "../config/config.php"; ?>
 
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
+    header('HTTP/1.0 403 Forbidden', TRUE, 403);
+    die(header('location: http://localhost:3000/bookstore'));
+}
+?>
 
-if(isset($_POST['delete'])){
-  
+<?php
+
+if (isset($_POST['delete'])) {
+
     $delete = $conn->prepare("DELETE FROM cart WHERE user_id='$_SESSION[user_id]'");
     $delete->execute();
 }
--- shopping/delete-item.php
+++ shopping/delete-item.php
@@ -2,6 +2,13 @@
 <?php require "../config/config.php"; ?>
 
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
+    header('HTTP/1.0 403 Forbidden', TRUE, 403);
+    die(header('location: http://localhost:3000/bookstore'));
+}
+?>
+
+<?php
 
 if(isset($_POST['delete'])){
     $id = $_POST['id'];
